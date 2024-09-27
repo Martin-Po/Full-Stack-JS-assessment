a104bc7438a3310a149b4f42062f28b64a0eab28
Martin-Po <85139462+Martin-Po@users.noreply.github.com>
2024-09-27 10:35:20 -0300

    Deleted unnecesary files

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,6 @@ const logger = require('./utils/logger')
 
 const server = http.createServer(app)
 
-console.log('aca');
-console.log(`aca2 puerto ${config.PORT}`)
 
 app.listen(config.PORT, () => {
     logger.info(`Server running on port ${config.PORT}`)
