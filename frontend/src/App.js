53fbf1206db3121e0c217b27e5e95e9c4aa47b81
Martin-Po <85139462+Martin-Po@users.noreply.github.com>
2024-09-27 16:04:58 -0300

    Added Home button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,37 @@
 import './App.css'
 import { CountriesList } from './components/countriesList'
 
+import { useState } from 'react'
 
-import {  useState } from 'react'
+import HomeIcon from '@mui/icons-material/Home';
 
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 import { CountryInfo } from './components/countryInfo'
+import { Box, IconButton } from '@mui/material'
 
 function App() {
-    const [loaded, setloaded] = useState(false)
+    const navigate = useNavigate()
+
+    const handleNavigation = (countryCode) => {
+        navigate(`/`)
+    }
 
     return (
         <div className="App">
-           <Routes>
-           <Route path="/info/:countryCode" element={<CountryInfo />} />
+            <Box sx={{ width: '100%', display: 'flex' }}>
+                <IconButton
+                    aria-label="delete"
+                    size="large"
+                    onClick={() => handleNavigation()}
+                >
+                    <HomeIcon fontSize="inherit" />
+                </IconButton>
+            </Box>
+            <Routes>
+                <Route path="/info/:countryCode" element={<CountryInfo />} />
 
-            <Route path="/*" element={<CountriesList  loaded = {loaded} />} />
-
-           </Routes>
+                <Route path="/*" element={<CountriesList />} />
+            </Routes>
         </div>
     )
 }
